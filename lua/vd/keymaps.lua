d212a1d8ca5c465e8707cafa128d76d280900766
Vignesh D <vignesh0025@gmail.com>
2022-10-23 23:25:07 +0530

    Add file_browser at current file directory

--- lua/vd/keymaps.lua
+++ lua/vd/keymaps.lua
@@ -14,7 +14,9 @@ local telescope_keymaps = function()
 	vim.keymap.set("n", "<leader>ev", function() builtin.fd{cwd="~/.config/nvim/"} end, bufopts)
 
 	local extensions = require('telescope').extensions
-	vim.keymap.set('n', '<c-p>f', function() extensions.file_browser.file_browser() end, {})
+	vim.keymap.set('n', '<c-p>f',extensions.file_browser.file_browser, {})
+	-- This actually maps <c-/> to fb at current open file directory
+	vim.keymap.set('n', '<c-_>', function() extensions.file_browser.file_browser({cwd=vim.fn.expand("%:h")}) end, {})
 	vim.keymap.set('n', '<c-Bslash>', extensions.live_grep_args.live_grep_args, {})
 end
 

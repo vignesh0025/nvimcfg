78070d8a675be27263a09d293611178cd748620f
Vignesh D <vignesh0025@gmail.com>
2023-02-05 21:13:05 +0530

    Update scroll keymap

--- lua/vd/lspsetup.lua
+++ lua/vd/lspsetup.lua
@@ -183,8 +183,8 @@ local config_nvimcmp = function()
 			})
 		},
 		mapping = cmp.mapping.preset.insert({
-			['<C-d>'] = cmp.mapping.scroll_docs(-4),
-			['<C-u>'] = cmp.mapping.scroll_docs(4),
+			['<C-u>'] = cmp.mapping.scroll_docs(-4),
+			['<C-d>'] = cmp.mapping.scroll_docs(4),
 			["<C-Space>"] = cmp.mapping.complete(),
 			["<C-e>"] = cmp.mapping.abort(),
 			["<CR>"] = cmp.mapping.confirm({ select = false }),

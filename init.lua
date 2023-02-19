7035ccbf254fbf6b1fb0661515aa36024c3b401b
Vignesh D <vignesh0025@gmail.com>
2023-02-19 12:36:09 +0530

    Update diagnostics icons

--- init.lua
+++ init.lua
@@ -37,3 +37,5 @@ vim.opt.swapfile = false
 vim.keymap.set("x",'/','<Esc>/\\%V')
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set('i', "jk", "<esc>")
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } for type, icon in pairs(signs) do local hl = "DiagnosticSign" .. type vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" }) end
